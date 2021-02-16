ebf23139b7a1482e4710e4f49fc038c39cd96a51
tufan35 <34610153+tufan35@users.noreply.github.com>
2021-02-16 16:49:51 +0300

    Veritabanı Güncellendi

--- ReCapProject/Program.cs
+++ ReCapProject/Program.cs
@@ -14,10 +14,10 @@ namespace ReCapProject
         static void Main(string[] args)
         {
             ////***************VİEW*********
-            //  CarTest();
-            // BrandTest();
-            //  ColorTest();
-
+  
+            //BrandTest();
+            //ColorTest();
+            //CarTest();
 
 
 
@@ -29,46 +29,16 @@ namespace ReCapProject
 
 
 
-            ///***************************************************////////////////////////
-            //  Car car1 = new Car()
-            //    {BrandId = 001, ColorId = 01, ModelYear = 2000, DailyPrice = 25000, Description = "Tofaş Şahin"};
-            //Car car2 = new Car()
-            //    { BrandId = 002, ColorId = 02, ModelYear = 2001, DailyPrice = 35000, Description = "Volswagen Polo" };
-            //Car car3 = new Car()
-            //    { BrandId = 003, ColorId = 03, ModelYear = 2002, DailyPrice = 55000, Description = "Renault Megane" };
-            //Car car4 = new Car()
-            //    { BrandId = 004, ColorId = 04, ModelYear = 2004, DailyPrice = 75000, Description = "Mercedes AMG" };
-            //Car car5 = new Car()
-            //    { BrandId = 005, ColorId = 05, ModelYear = 2005, DailyPrice = 10000, Description = "Fiat Linea" };
 
-
-
-            //*************İNSERT***
-            //  DbInsert(carService, car1,car2,car3,car4,car5);
-            // AddTest(carManager, brandManager, colorManager);
+           // AddTest(carManager, brandManager, colorManager);
             UpdateTest(carManager, brandManager, colorManager);
 
 
-
+        
             Console.ReadLine();
         }
 
 
-        private static void DbInsert(ICarService carService, Car car1, Car car2, Car car3, Car car4, Car car5)
-        {
-            Console.WriteLine("************ Araçlar veritabanına Kaydedilmektedir.************");
-
-            List<Car> cars = new List<Car> { car1, car2, car3, car4, car5 };
-
-            int say = 1;
-
-            foreach (var car in cars)
-            {
-                Console.WriteLine(say + "." + car.Description + " Aracı Kaydedilmiştir. ");
-                say = say + 1;
-            }
-
-        }
 
 
         private static void CarTest()
@@ -112,21 +82,24 @@ namespace ReCapProject
         private static void AddTest(CarManager carManager, BrandManager brandManager, ColorManager colorManager)
         {
             Console.WriteLine("-------------Veri tabanına Eklenme--------------");
-            carManager.Add(new Car { BrandId = 2, ColorId = 3, ModelYear = 2015, DailyPrice = 500, Description = "Kartal" });
-            brandManager.Add(new Brand { BrandName = "TOFAŞ" });
+          
+            brandManager.Add(new Brand { BrandName = "FİAT" });
             colorManager.Add(new Color { ColorName = "GRİ" });
+            carManager.Add(new Car { BrandId = 4, ColorId = 4, ModelYear = 2015, DailyPrice = 500, Description = "FİAT DOBLO" });
+
+
+
         }
 
 
         private static void UpdateTest(CarManager carManager, BrandManager brandManager, ColorManager colorManager)
         {
             Console.WriteLine("------------Veritabanı Güncelleme---------");
-            carManager.Update(new Car { Id = 1007, BrandId = 2, ColorId = 1003, ModelYear = 2016, DailyPrice = 550, Description = "TOYOTA" });
-            brandManager.Update(new Brand { BrandId = 1003, BrandName = "ISUZU" });
-            colorManager.Update(new Color { ColorId = 1003, ColorName = "Mavi" });
+            brandManager.Update(new Brand { BrandId = 4, BrandName = "LİNEA" });
+            colorManager.Update(new Color { ColorId = 2, ColorName = "SİYAH" });
+            carManager.Update(new Car { Id = 5, BrandId = 4, ColorId = 2, ModelYear = 2000, DailyPrice = 450, Description = "FİAT LİNEA" });
+
         }
     }
 }
 
-
-///veri tabanı güncellemesi, kontrol edilecektri tabloları normalizasyon yapılmıştır. veriler güncellenecektir.
